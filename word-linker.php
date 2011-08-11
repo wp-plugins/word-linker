a7c0740e2467552d5fed718326dc1be88dac7b87
dangerousprototypes <dangerousprototypes@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2011-08-11 15:26:45 +0000

    fix bug
    git-svn-id: https://plugins.svn.wordpress.org/word-linker/trunk@422200 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- word-linker.php
+++ word-linker.php
@@ -23,7 +23,7 @@ Version: 0.0.1
     You should have received a copy of the GNU General Public License
     along with this program; if not, write to the Free Software
     Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
-
+*/
 
 
 add_action('admin_menu', 'create_menu');
